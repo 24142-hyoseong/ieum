6445ca18a7e92b787a76bafc4a9f2c0481f37587
24196-creator <24196@unjung.hs.kr>
2026-05-26 12:59:14 +0900

    parser 테스트 기본 구조 추가

---
+++ tests/test_parser.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestParser(unittest.TestCase):
+    """Parser 테스트 (추후 구현 예정)"""
+
+    def test_placeholder(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
